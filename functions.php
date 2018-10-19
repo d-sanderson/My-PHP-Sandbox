eab7d40436839fa2e5211bb4b5626c3aa00dfa81
David Sanderson <dsanderson3@cnm.edu>
2018-10-19 08:15:55 -0600

    Basic Utility Calculator in PHP

---
+++ functions.php
@@ -0,0 +1,35 @@
+<?php
+
+
+$mySnapArray = array(1,2,3,5,8,13,21,34,55,89,144);
+
+echo array_sum($mySnapArray);
+
+function square($n) {
+	return($n * $n);
+}
+
+$arraysquared = array_map('square', $mySnapArray);
+
+print_r($arraysquared);
+
+
+function sayHi($name, $age){
+	echo "Hello $name, you are $age";
+}
+
+sayHi("David", 24);
+sayHi("Dude", 13);
+sayHi("Jeffrey", 58);
+
+function cube($number) {
+	return $number * $number * $number;
+}
+$cubeResult = cube(4);
+
+echo $cubeResult;
+
+
+
+
+?>
